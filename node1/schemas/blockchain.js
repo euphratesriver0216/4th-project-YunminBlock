a82b9411a9165c6666e2bd87c564fbc66236b87d
ararararararara <jiang46@naver.com>
2022-01-12 16:04:08 +0900

    f
    ffffdd

---
+++ node1/schemas/blockchain.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const userSchema = new Schema({
+    version:{ 
+     type: Number,
+     required: true,
+    },
+    index: {
+    type: Number,
+    required: true,
+  },
+  previousHash: {
+    type: String,
+    required: false,
+  },
+  timestamp: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  merkleRoot: {
+    type: String,
+    required: true,
+  },
+  difficulty: {
+    type: Number,
+    required: true,
+  },
+  hash: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('BlockChain', BlockChainSchema);
