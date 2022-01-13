7a1ea62f27a411fc41025c056a424cb70d6e0c20
CHOI HYUN SEOK <jiang46@naver.com>
2022-01-13 09:28:30 +0900

    ss
    ss

--- node1/schemas/blockchain.js
+++ node1/schemas/blockchain.js
@@ -1,36 +1,53 @@
-// const mongoose = require('mongoose');
+const Sequelize = require("sequelize");
 
-// const { Schema } = mongoose;
-// const userSchema = new Schema({
-//     version:{ 
-//      type: Number,
-//      required: true,
-//     },
-//     index: {
-//     type: Number,
-//     required: true,
-//   },
-//   previousHash: {
-//     type: String,
-//     required: false,
-//   },
-//   timestamp: {
-//     type: Date,
-//     required: true,
-//     default: Date.now,
-//   },
-//   merkleRoot: {
-//     type: String,
-//     required: true,
-//   },
-//   difficulty: {
-//     type: Number,
-//     required: true,
-//   },
-//   hash: {
-//     type: String,
-//     required: true,
-//   },
-// });
-
-// module.exports = mongoose.model('BlockChain', BlockChainSchema);
+module.exports = class Blockchain extends Sequelize.Model {
+    static init(sequelize) {
+        return super.init(
+            {
+                version: {
+                    type: Sequelize.DOUBLE,
+                    allowNull: true,
+                    unique: true,
+                },
+                index: {
+                    type: Sequelize.INTEGER,
+                    allowNull: false,
+                },
+                previousHash: {
+                    type: Sequelize.VIRTUAL,
+                    allowNull: true,
+                },
+                timestamp: {
+                    type: Sequelize.STRING,
+                    allowNull: true,
+                    unique: true,
+                },
+                merkleRoot: {
+                    type: Sequelize.VIRTUAL,
+                    allowNull: true,
+                    unique: true,
+                },
+                difficulty: {
+                    type: Sequelize.INTEGER,
+                    allowNull: true,
+                },
+                nonce: {
+                    type: Sequelize.INTEGER,
+                    allowNull: true,
+                    unique: true,
+                },
+            },
+            {
+                sequelize,
+                timestamps: false,
+                underscored: false,
+                modelName: "Blockchain",
+                tableName: "blockchains",
+                paranoid: true,
+                charset: "utf8",
+                collate: "utf8_general_ci",
+            }
+        );
+    }
+    static associate(db) {}
+};
