156fd0b31b93db9f51a72f81610c0489cf4b1119
ming <euphratesmin@gmail.com>
2022-01-13 16:00:12 +0900

    CRA-version

---
+++ cra1/src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return <div>윤민블럭 준비 완료</div>;
+  }
+}
+
+export default App;
---
+++ cra1/src/components/pages/Mainpage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Mainpage = () => {
+  return (
+    <div>
+      <h2>윤민블럭</h2>
+    </div>
+  );
+};
+
+export default Mainpage;
