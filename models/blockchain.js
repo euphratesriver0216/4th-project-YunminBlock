b5e821bdc56f71ca82a8bcc5435948f899bfe6ab
jsoyun <syusmm233@gmail.com>
2022-01-13 17:45:30 +0900

    db저장

--- models/blockchain.js
+++ models/blockchain.js
@@ -4,45 +4,17 @@ module.exports = class Blockchain extends Sequelize.Model {
   static init(sequelize) {
     return super.init(
       {
-        version: {
-          type: Sequelize.DOUBLE,
-          allowNull: true,
-          unique: true,
-        },
-        index: {
-          type: Sequelize.INTEGER,
+        Blockchain: {
+          type: Sequelize.JSON,
           allowNull: false,
-        },
-        previousHash: {
-          type: Sequelize.STRING,
-          allowNull: true,
-        },
-        timestamp: {
-          type: Sequelize.STRING,
-          allowNull: true,
-          unique: true,
-        },
-        merkleRoot: {
-          type: Sequelize.STRING,
-          allowNull: true,
-          unique: true,
-        },
-        difficulty: {
-          type: Sequelize.INTEGER,
-          allowNull: true,
-        },
-        nonce: {
-          type: Sequelize.INTEGER,
-          allowNull: true,
-          unique: true,
         },
       },
       {
         sequelize,
         timestamps: false,
         underscored: false,
-        modelName: "Blockchain",
-        tableName: "blockchains",
+        // modelName: "Blockchain",
+        // tableName: "blockchains",
         paranoid: true,
         charset: "utf8",
         collate: "utf8_general_ci",
--- node1/r_util.js
+++ node1/r_util.js
@@ -1,33 +1,39 @@
-//db저장하는 함수
-const blockchain = require("../models/blockchain");
-function addDB(newBlock) {
-  console.log("바디데이터", newBlock);
-  const {
-    version,
-    index,
-    previousHash,
-    timestamp,
-    merkleRoot,
-    difficulty,
-    nonce,
-  } = newBlock.header;
-  console.log("바디데이터의 헤더", newBlock.header);
-  blockchain
-    .create({
-      version: version,
-      index: index,
-      previousHash: previousHash,
-      timestamp: timestamp,
-      merkleRoot: merkleRoot,
-      difficulty: difficulty,
-      nonce: nonce,
-    })
-    .then(() => {
-      console.log("블록db저장 성공");
-    })
-    .catch((err) => {
-      console.log("블록 db 저장실패", err);
-    });
-}
+// //db저장하는 함수
+// const blockchain = require("../models/blockchain");
 
-module.exports = { addDB };
+// function addDB(newBlock) {
+//   const { Blocks, getBlocks } = require("./r_blockchain");
+//   console.log("-----", getBlocks());
+//   console.log("----------------------------", Blocks);
+//   console.log("바디데이터", newBlock);
+//   console.log(typeof newBlock.header.index);
+//   console.log(newBlock.header.index);
+//   const {
+//     version,
+//     index,
+//     previousHash,
+//     timestamp,
+//     merkleRoot,
+//     difficulty,
+//     nonce,
+//   } = newBlock.header;
+//   console.log("바디데이터의 헤더", newBlock);
+//   blockchain
+//     .create({
+//       version: version,
+//       index: index,
+//       previousHash: previousHash,
+//       timestamp: timestamp,
+//       merkleRoot: merkleRoot,
+//       difficulty: difficulty,
+//       nonce: nonce,
+//     })
+//     .then(() => {
+//       console.log("블록db저장 성공");
+//     })
+//     .catch((err) => {
+//       console.log("블록 db 저장실패", err);
+//     });
+// }
+
+// module.exports = { addDB };
