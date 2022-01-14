db84d2a57c9a52bc871df42704f729b0b452cdaf
ming <euphratesmin@gmail.com>
2022-01-15 04:14:44 +0900

    UI수정

--- server/node1/r_util.js
+++ server/node1/r_util.js
@@ -1,9 +1,9 @@
-const db = require("../models");
+const db = require("../../models");
 
 //db띄우는함수
 async function importBlockDB() {
   //   const { Block, BlockHeader, Blocks } = require("./r_blockchain");
-  const Blockchain = require("../models/blockchain");
+  const Blockchain = require("../../models/blockchain");
 
   const test = await Blockchain.findAll({ raw: true });
   console.log("->", test);
