f64e9152d89e220f78b8cec13b0038c2dce21011
jsoyun <syusmm233@gmail.com>
2022-01-14 16:11:17 +0900

    db정보만 띄우게함

--- node1/r_util.js
+++ node1/r_util.js
@@ -5,7 +5,7 @@ async function importBlockDB() {
   //   const { Block, BlockHeader, Blocks } = require("./r_blockchain");
   const Blockchain = require("../models/blockchain");
 
-  const test = await Blockchain.findAll();
+  const test = await Blockchain.findAll({ raw: true });
   console.log("->", test);
   return test;
 }
