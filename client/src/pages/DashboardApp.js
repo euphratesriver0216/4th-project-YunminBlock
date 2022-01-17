4a4f69f8964b9c607dd30c1bed67dedf7976aee2
ming <euphratesmin@gmail.com>
2022-01-17 10:42:33 +0900

    react 가능함

--- client/src/pages/DashboardApp.js
+++ client/src/pages/DashboardApp.js
@@ -25,7 +25,7 @@ export default function DashboardApp() {
       <Container maxWidth="xl">
         <Box sx={{ pb: 5 }}>
           <Typography variant="h4">Hi, This is YUNMIN Block! </Typography>
-          <Button>SHOW ME THE BLOCK</Button>
+          <Button>START TO BLOCK MINING</Button>
         </Box>
         <Grid container spacing={3}>
           <Grid item xs={12} sm={6} md={3}>
